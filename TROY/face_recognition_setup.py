d0025188bfaf1117a53d273300a732c7b3d2e897
SRI VISHNU JSB <SRIVISHNU.JSB@PRICELINE.COM>
2023-10-14 20:13:27 +0530

    Initial Commit of macOS Version

--- TROY/face_recognition_setup.py
+++ TROY/face_recognition_setup.py
@@ -1,22 +1,34 @@
 import cv2
 import face_recognition
 import pickle
+import time
 
 def capture_image():
     cap = cv2.VideoCapture(0)  # 0 for default camera
+    time.sleep(2)  # Wait for 2 seconds to let the camera initialize
     ret, frame = cap.read()
-    cv2.imwrite("user_face.jpg", frame)
+    if ret:
+        cv2.imwrite("user_face.jpg", frame)
+    else:
+        print("Error: Could not capture an image.")
     cap.release()
     cv2.destroyAllWindows()
 
 def generate_face_encoding(image_path):
     image = face_recognition.load_image_file(image_path)
-    face_encoding = face_recognition.face_encodings(image)[0]
-    return face_encoding
+    face_encodings = face_recognition.face_encodings(image)
+    if len(face_encodings) > 0:
+        return face_encodings[0]
+    else:
+        print("No faces found in the image.")
+        return None
 
 def save_face_encoding(face_encoding, filename="face_encoding.pkl"):
-    with open(filename, "wb") as f:
-        pickle.dump(face_encoding, f)
+    if face_encoding is not None:
+        with open(filename, "wb") as f:
+            pickle.dump(face_encoding, f)
+    else:
+        print("Face encoding not saved as it is None.")
 
 def initial_configuration():
     capture_image()
